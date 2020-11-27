719eae3e397a73caf6f293b051a34b3e5d9a7edc
Vincent Russell <vincent.russell@gmail.com>
2020-11-26 22:47:40 -0500

    adding docker support

---
+++ src/main/docker/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+umask 0002
+
+java_default_props=("file.separator" "java.class.path" "java.home" "java.vendor" "java.vendor.url" "java.version" "line.separator" "os.arch" "os.name" "os.version" "path.separator" "user.dir" "user.home" "user.name" "_" "JAVA_HOME" "JAVA_VERSION" "HOSTNAME" "PATH" "HOME" "SHLVL" "PWD" "JAVA_OPTS")
+
+declare -a j_opts
+
+j_opts+=("-Dspring.profiles.active=default")
+
+while IFS='=' read -r envvar_key envvar_value
+do
+    if [[ " ${java_default_props[@]} " =~ "${envvar_key}" ]]; then
+      echo ""
+    else
+      if [[ ! -z $envvar_value ]]; then
+        if [[ "$envvar_value" =~ \ |\' ]]; then
+            j_opt="-D$envvar_key=\"$envvar_value\""
+        else
+            j_opt="-D$envvar_key=$envvar_value"
+        fi
+        j_opts+=("${j_opt}")
+      fi
+    fi
+done < <(env)
+
+echo "------------------------------"
+echo "${j_opts[@]}"
+echo "------------------------------"
+
+java -jar "${j_opts[@]}" "/app.jar"
